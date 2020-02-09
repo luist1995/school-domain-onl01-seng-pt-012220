f3ee75a4b2ec8e325081b3edc7b520f415d88a50
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-02-09 00:43:07 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,21 +1,23 @@
 class School
 
-attr_reader :name, :roster
+  attr_reader :name, :roster
 
-def initialize(name)
-  @name = name
-  @roster = Hash.new {|h, k| h[k] = []}
-end
+  def initialize(name)
+   @name = name
+   @roster = Hash.new {|h, k| h[k] = []}
+  end
 
-def roster
-  @roster
-end
+  def roster
+    @roster
+  end
 
-def add_student (name, grade)
-  self.roster[grade] << name
-end
+  def add_student (name, grade)
+    self.roster[grade] << name
+  end
   
   def grade(grade)
     self.roster[grade]
   end
+  
+  def 
 end
